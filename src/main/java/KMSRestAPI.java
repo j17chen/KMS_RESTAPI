03e0ade86be4a5ef68a03d05a3996e7aedcb7975
jchen <jchen@hortonworks.com>
2018-02-26 03:28:38 -0800

    change name

--- src/main/java/KMSRestAPI.java
+++ src/main/java/KMSRestAPI.java
@@ -4,29 +4,13 @@ import org.apache.http.entity.StringEntity;
 /**
  * Created by jchen on 24/02/2018.
  */
-public class kmsREST {
+public class KMSRestAPI {
     private static final String KMS_BASE_URI="http://node3.openstacklocal.com:9292";
     private static final String KMS_KEYTAB="/Users/jchen/keyadmin.keytab";
     private static final String KMS_PRINCIPAL="keyadmin@HWX.COM";
     private static KerberosHttpClient kerberosHttpClient = new KerberosHttpClient(KMS_KEYTAB, KMS_PRINCIPAL, ServiceNameType.HOST_BASED);
 
-    public static void main(String[] args) {
-
-        String result;
-        //add key
-        result=KMSREST_addKey();
-        if (result!=null) {
-            System.out.println("Result For add Keys: " + result);
-        }
-
-        //get key
-        result=KMSREST_getKeys();
-        if (result!=null) {
-            System.out.println("Result For Get Keys: " + result);
-        }
-    }
-
-    private static String KMSREST_getKeys(){
+    private static String KMSRestAPI_getKeys(){
         String result = null;
         try {
             result = kerberosHttpClient.executeGet(KMS_BASE_URI + "/kms/v1/keys/names");
@@ -36,13 +20,12 @@ public class kmsREST {
             ex.printStackTrace();
             return null;
         }
-
     }
 
-    private static String KMSREST_addKey(){
+    private static String KMSRestAPI_addKey(){
         String result = null;
         try {
-            String reqestData="{\n" +
+            String requestData="{\n" +
                     "  \"name\"        : \"test_key7\",\n" +
                     "  \"cipher\"      : \"aes\",\n" +
                     "  \"length\"      : 128, \n" +
@@ -50,7 +33,7 @@ public class kmsREST {
                     "  \"description\" : \"test key6\"\n" +
                     "}";
 
-            StringEntity entity = new StringEntity(reqestData,"utf-8");
+            StringEntity entity = new StringEntity(requestData,"utf-8");
             entity.setContentEncoding("UTF-8");
             entity.setContentType("application/json");
             HttpPost httpPost = new HttpPost(KMS_BASE_URI+"/kms/v1/keys");
@@ -62,5 +45,25 @@ public class kmsREST {
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
-        }    }
+        }
+    }
+    public static void main(String[] args) {
+
+        String result;
+        //add key
+        result= KMSRestAPI_addKey();
+        if (result!=null) {
+            System.out.println("Result For add Keys: " + result);
+        }
+
+        //get key
+        result= KMSRestAPI_getKeys();
+        if (result!=null) {
+            System.out.println("Result For Get Keys: " + result);
+        }
+    }
+
+
+
+
 }
